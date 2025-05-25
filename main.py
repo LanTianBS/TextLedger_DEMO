473aa8e05bdd1f27015056032caccd49f1863182
LanTianBS <105405617+LanTianBS@users.noreply.github.com>
2025-05-25 12:52:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from app import app
 logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5100, debug=True)
 
 
 
